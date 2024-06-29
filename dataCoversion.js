0b02c85064ee71af85a2b6eeebbc11d536fc0bfb
abhishek <abhisheksingh961931@gmail.com>
2024-06-29 11:47:56 +0530

    Initial commit

---
+++ dataCoversion.js
@@ -0,0 +1,13 @@
+let score="33a"
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof(score))
+console.log(valueInNumber)
+
+let score1=true
+console.log(typeof(score1))
+
+let valueInNumber1=Number(score1)
+console.log(typeof(score1))
+console.log(valueInNumber1)
