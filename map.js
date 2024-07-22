97a2f414312a5ea13c371fd83f2710b31ff97e60
abhishek <abhisheksingh961931@gmail.com>
2024-07-22 22:03:29 +0530

    full map

--- map.js
+++ map.js
@@ -1,5 +1,14 @@
 const input = [1,2,3,4,5,6,7,8,9,10];
 
+// Using for loop to create a new array which is the traditional way to put .
+
+let newarray=[];
+for (let i = 0; i < input.length; i++) {
+    newarray.push(input[i]*5);
+    
+}
+console.log(newarray);
+
 // Using map() function to create a new array
 
 function transform(i){
@@ -8,3 +17,17 @@ function transform(i){
 const ans=input.map(transform)
 
 console.log(ans);
+
+//another way to do this using map function
+
+const ans2=input.map(function(i){
+    return i*3;
+})
+console.log(ans2);
+
+//another way to do this using arrow and map function
+
+const ans3=input.map((i)=>{
+    return i*4;})
+    
+console.log(ans3);
